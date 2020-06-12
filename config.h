ba284bc3158e53a551b56a11a9d4457cc47b16e9
nagbalae <nbg.hun@gmail.com>
2020-06-12 16:29:35 +0200

    Fixed comp error

--- config.h
+++ config.h
@@ -75,7 +75,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER")},
-	{ MODKEY,                       XK_r,      spawn,          SHCMD("$TERM -e ranger"},
+	{ MODKEY,                       XK_r,      spawn,          SHCMD("$TERM -e ranger")},
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
